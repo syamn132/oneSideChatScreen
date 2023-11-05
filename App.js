ff0baa761835b3b81e639f300103ee28d19af445
syamn132 <ssrn132@gmail.com>
2023-11-05 17:23:36 +0530

    Updated the Profile Pic Updation

--- App.js
+++ App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ChatScreen from './app/screens/ChatScreen';
-import ProfilePicUpdate from './app/screens/ProfilePicUpdate';
 import HomeScreen from './app/screens/HomeScreen';
-import CameraAccess from './app/screens/CameraAccess';
 
 const Stack = createStackNavigator();
 
@@ -14,7 +12,6 @@ function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
         <Stack.Screen name="ChatScreen" component={ChatScreen} />
-        <Stack.Screen name="ProfilePicUpdate" component={ProfilePicUpdate} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- app/screens/ChatScreen.js
+++ app/screens/ChatScreen.js
@@ -1,113 +1,161 @@
 import React from 'react';
 import { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, ScrollView, Image, TouchableOpacity, Dimensions } from 'react-native';
+import { View, Text, TextInput, StyleSheet, ScrollView, Image, TouchableOpacity, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';
 
 import colors from '../config/colors';
 
 const ChatScreen = () => {
-  const [messages, setMessages] = useState([
-    {type: 'text', text: 'Hello there!', time: '10:30 AM', img: null}
-  ]);
-  const navigation = useNavigation();
-  const [newMessage, setNewMessage] = useState('');
-  const [selectedImage, setSelectedImage] = useState(null);
+    const navigation = useNavigation();
 
-  const pickImage = async () => {
-    const result = await ImagePicker.launchCameraAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-        const imageUri = result.uri;
-        setSelectedImage(imageUri);
-        const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-        setMessages([...messages, { type: 'image', time: currentTime, img: imageUri }]);
-      }
-    };
-
+    const [newMessage, setNewMessage] = useState('');
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [profilePicture, setProfilePicture] = useState('https://p.kindpng.com/picc/s/394-3947019_round-profile-picture-png-transparent-png.png');
+    const [showExtraContent, setShowExtraContent] = useState(false);
+    const [messages, setMessages] = useState([
+      {type: 'text', text: 'Hello there!', time: '10:30 AM', img: null}
+    ]);
+  
     const addMessage = () => {
-    if (newMessage.trim() !== '') {
-        const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-        setMessages([...messages, { type: 'text', text: newMessage, time: currentTime, img: null }]);
-        setNewMessage('');
-    } 
+      if (newMessage.trim() !== '') {
+          const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+          setMessages([...messages, { type: 'text', text: newMessage, time: currentTime, img: null }]);
+          setNewMessage('');
+        } 
     };
+    
+    const captureImage = async () => {
+        const result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            quality: 1,
+        });
+        if (!result.canceled) {
+            const imageAsset = result.assets[0];
+            const imageUri = imageAsset.uri;
+            setSelectedImage(imageUri);
+            const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+            setMessages([...messages, { type: 'image', time: currentTime, img: imageUri }]);
+        }
+    };
+
+    const attachImage = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          quality: 1,
+        });
+        if (!result.canceled) {
+            const imageAsset = result.assets[0];
+            const imageUri = imageAsset.uri;
+            setSelectedImage(imageUri);
+            const currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+            setMessages([...messages, { type: 'image', time: currentTime, img: imageUri }]);
+            }
+    };
+
+    const profilepic = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [3, 3],
+            quality: 1,
+        });
+        if (!result.canceled) {
+            const imageAsset = result.assets[0];
+            setProfilePicture(imageAsset.uri);
+        }
+    };
+
+    const showProfile = () => {
+        setShowExtraContent(!showExtraContent);
+    };
+
 
 return (
     <View style={styles.container}>
+        {showExtraContent && (
+                <View style={styles.profilePicOpenScreen}>
+                {<Image source={{uri : profilePicture}} style={styles.image} />}
+                <View style={styles.buttons}>
+                    <Button color={colors.primary} title="Change" onPress={profilepic} />
+                    <View style={{ width: 40 }} />
+                    <Button color={colors.primary} title="Upload" onPress={showProfile}/>
+                </View>
+                </View>
+        )}
+        {!showExtraContent && ( 
+        <>
         <ScrollView style={{ flex: 1 }} contentContainerStyle={styles.messageContainer}>
         {messages.map((message, index) => (
             <View key={index}>
             {message.type === 'text' && (
-          <View style={styles.message}>
-            <Text style={styles.messageText}>{message.text}</Text>
-          </View>
-        )}
-        {message.type === 'image' && (
-          <View style={styles.photo}>
-            <Image source={{ uri: message.img }} style={{ width: '70%', aspectRatio: 4/3 }} />
-          </View>
-        )}
-                <Text style={styles.messageTime}>{message.time}</Text>
+                <View style={styles.message}>
+                    <Text style={styles.messageText}>{message.text}</Text>
+                </View>
+            )}
+            {message.type === 'image' && (
+                <View style={styles.photo}>
+                    <Image source={{ uri: message.img }} style={{ width: '70%', aspectRatio: 4/3}} />
+                </View>
+            )}
+            <Text style={styles.messageTime}>{message.time}</Text>
             </View>
         ))}
         </ScrollView>
         <View style={styles.profileContainer}>
-            <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
-            <Image
-            style={styles.backarrow}
-            source={require('../assets/back.png')}
-            >
-            </Image>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('ProfilePicUpdate')}>
-            <Image
-            style={styles.profilepic}
-            source={require('../assets/ppr.png')}
-            >
-            </Image>
-            </TouchableOpacity>
-            <Text style={styles.profileName}>Emma Watson</Text>
-            <Image
-            style={styles.callbtn}
-            source={require('../assets/call.png')}
-            >
-            </Image>
-            <Image
-            style={styles.vcallbtn}
-            source={require('../assets/vcall.png')}
-            >
-            </Image>
-        </View>
-        <View style={styles.inputContainer}>
-        <TextInput
-            style={styles.input}
-            placeholder="Type your message"
-            value={newMessage}
-            onChangeText={text => setNewMessage(text)}
-        />
-        <TouchableOpacity onPress={pickImage}>
-            <Image
-                source={require('../assets/camera.png')}
-                style = {styles.camera}
-            >
-            </Image>
-        </TouchableOpacity>
-        <Image
-        style={styles.attach}
-        source={require('../assets/attach.png')}
-        >
-        </Image>
-        <TouchableOpacity onPress={addMessage}>
-        <Image
-            source={require('../assets/send.png')}
-            style={styles.send}
-        />
-        </TouchableOpacity>
-        </View>
+                <TouchableOpacity onPress={() => navigation.navigate('HomeScreen')}>
+                    <Image
+                        style={styles.backarrow}
+                        source={require('../assets/back.png')}
+                    >
+                    </Image>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={showProfile}>
+                    <Image
+                        style={styles.profilepic}
+                        source={{ uri: profilePicture }}
+                    >
+                    </Image>
+                </TouchableOpacity>
+                <Text style={styles.profileName}>Emma Watson</Text>
+                <Image
+                    style={styles.callbtn}
+                    source={require('../assets/call.png')}
+                >
+                </Image>
+                <Image
+                    style={styles.vcallbtn}
+                    source={require('../assets/vcall.png')}
+                >
+                </Image>
+            </View>
+            <View style={styles.inputContainer}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Type your message"
+                        value={newMessage}
+                        onChangeText={text => setNewMessage(text)} />
+                    <TouchableOpacity onPress={captureImage}>
+                        <Image
+                            source={require('../assets/camera.png')}
+                            style={styles.camera}
+                        >
+                        </Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={attachImage}>
+                    <Image
+                        style={styles.attach}
+                        source={require('../assets/attach.png')}
+                    >
+                    </Image>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={addMessage}>
+                        <Image
+                            source={require('../assets/send.png')}
+                            style={styles.send} />
+                    </TouchableOpacity>
+                </View></>
+        )}
     </View>
     );
 };
@@ -121,6 +169,10 @@ const styles = StyleSheet.create({
     backarrow:{
         height: 40,
         width: 40,
+    },
+
+    buttons: {
+        flexDirection: 'row',
     },
 
     callbtn: {
@@ -143,6 +195,12 @@ const styles = StyleSheet.create({
         alignSelf: 'flex-end',
         borderWidth: 1,
         borderColor: 'black',
+    },
+
+    image: {
+        width: 300,
+        height: 300,
+        marginBottom: 15,
     },
 
     inputContainer: {
@@ -204,10 +262,21 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
 
+    
     profilepic: {
         height: 55,
         width: 55,
+        borderRadius: 30
     },
+
+    profilePicOpenScreen: {
+        flex: 1,
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
     profileName: {
         fontSize: 20,
         color: 'white',
